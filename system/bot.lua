e9c2f971cb0058a1ed31c32f9078c2ecb5f2ba4c
Tele-Sudo <shahnazghavidel123@gmail.com>
2016-12-01 00:43:05 -0800

    TeleDiamond

--- system/bot.lua
+++ system/bot.lua
@@ -237,60 +237,7 @@ https://github.com/Mohammadrezar/TD.git
 Id Channel:
 @antispamandhack
 ]],
-    help_text_realm = [[
-Realm Commands:
-!creategroup [Name]
-Create a group
-!createrealm [Name]
-Create a realm
-!setname [Name]
-Set realm name
-!setabout [group|sgroup] [GroupID] [Text]
-Set a group's about text
-!setrules [GroupID] [Text]
-Set a group's rules
-!lock [GroupID] [setting]
-Lock a group's setting
-!unlock [GroupID] [setting]
-Unock a group's setting
-!settings [group|sgroup] [GroupID]
-Set settings for GroupID
-!wholist
-Get a list of members in group/realm
-!who
-Get a file of members in group/realm
-!type
-Get group type
-!kill chat [GroupID]
-Kick all memebers and delete group
-!kill realm [RealmID]
-Kick all members and delete realm
-!addadmin [id|username]
-Promote an admin by id OR username *Sudo only
-!removeadmin [id|username]
-Demote an admin by id OR username *Sudo only
-!list groups
-Get a list of all groups
-!list realms
-Get a list of all realms
-!support
-Promote user to support
-!-support
-Demote user from support
-!log
-Get a logfile of current group or realm
-!broadcast [text]
-!broadcast Hello !
-Send text to all groups
-Only sudo users can run this command
-!bc [group_id] [text]
-!bc 123456789 Hello !
-This command will send text to [group_id]
-**You can use "#", "!", or "/" to begin all commands
-*Only admins and sudo can add bots in group
-*Only admins and sudo can use kick,ban,unban,newlink,setphoto,setname,lock,unlock,set rules,set about and settings commands
-*Only admins and sudo can use res, setowner, commands
-]],
+    help_text_realm = [[]],
     help_text = [[]],
 	help_text_super =[[]],
   }
