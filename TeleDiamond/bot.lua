6deca75a5ebc22b261683c17d5aa54b3087f4d64
Tele-Sudo <shahnazghavidel123@gmail.com>
2016-12-03 23:20:24 +0330

    TeleDiamond

--- TeleDiamond/bot.lua
+++ TeleDiamond/bot.lua
@@ -4,10 +4,11 @@ package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 
 require("./TeleDiamond/commands")
 
-local f = assert(io.popen('/usr/bin/git describe —tags', 'r'))
+local f = assert(io.popen('/usr/bin/git describe --tags', 'r'))
 VERSION = assert(f:read('*a'))
 f:close()
 
+-- This function is called when tg receive a msg
 function on_msg_receive (msg)
   if not started then
     return
@@ -17,8 +18,8 @@ function on_msg_receive (msg)
 
   local receiver = get_receiver(msg)
   print(receiver)
-  —vardump(msg)
-  —vardump(msg)
+  --vardump(msg)
+  --vardump(msg)
   msg = pre_process_service_msg(msg)
   if msg_valid(msg) then
     msg = pre_process_msg(msg)
@@ -40,23 +41,24 @@ end
 function on_binlog_replay_end()
   started = true
   postpone (cron_plugins, false, 60*5.0)
-  — See plugins/isup.lua as an example for cron
+  -- See plugins/isup.lua as an example for cron
 
   _config = load_config()
 
-  — load plugins
+  -- load plugins
   plugins = {}
   load_plugins()
 end
 
 function msg_valid(msg)
-  — Don't process outgoing messages
+  -- Don't process outgoing messages
   if msg.out then
     print('\27[36mNot valid: msg from us\27[39m')
     return false
   end
 
-  — Before bot was started
+  -- Before bot was started
+
   if msg.unread == 0 then
     print('\27[36mNot valid: readed\27[39m')
     return false
@@ -83,21 +85,21 @@ function msg_valid(msg)
   end
 
   if msg.from.id == 777000 then
-    —send_large_msg(*group id*, msg.text) *login code will be sent to GroupID*
+    --send_large_msg(*group id*, msg.text) *login code will be sent to GroupID*
     return false
   end
 
   return true
 end
 
-—
+--
 function pre_process_service_msg(msg)
    if msg.service then
       local action = msg.action or {type=""}
-      — Double ! to discriminate of normal actions
+      -- Double ! to discriminate of normal actions
       msg.text = "!!tgservice " .. action.type
 
-      — wipe the data to allow the bot to read service messages
+      -- wipe the data to allow the bot to read service messages
       if msg.out then
          msg.out = false
       end
@@ -108,30 +110,30 @@ function pre_process_service_msg(msg)
    return msg
 end
 
-— Apply plugin.pre_process function
+-- Apply plugin.pre_process function
 function pre_process_msg(msg)
   for name,plugin in pairs(plugins) do
     if plugin.pre_process and msg then
-      —print('Preprocess: ', name)
+      print('Preprocess', name)
       msg = plugin.pre_process(msg)
     end
   end
   return msg
 end
 
-— Go over enabled plugins patterns.
+-- Go over enabled plugins patterns.
 function match_plugins(msg)
   for name, plugin in pairs(plugins) do
     match_plugin(plugin, name, msg)
   end
 end
 
-— Check if plugin is on _config.disabled_plugin_on_chat table
+-- Check if plugin is on _config.disabled_plugin_on_chat table
 local function is_plugin_disabled_on_chat(plugin_name, receiver)
   local disabled_chats = _config.disabled_plugin_on_chat
-  — Table exists and chat has disabled plugins
+  -- Table exists and chat has disabled plugins
   if disabled_chats and disabled_chats[receiver] then
-    — Checks if plugin is disabled on this chat
+    -- Checks if plugin is disabled on this chat
     for disabled_plugin,disabled in pairs(disabled_chats[receiver]) do
       if disabled_plugin == plugin_name and disabled then
         local warning = 'Plugin '..disabled_plugin..' is disabled on this chat'
@@ -147,17 +149,18 @@ end
 function match_plugin(plugin, plugin_name, msg)
   local receiver = get_receiver(msg)
 
-  — Go over patterns. If one matches it's enough.
+  -- Go over patterns. If one matches it's enough.
   for k, pattern in pairs(plugin.patterns) do
     local matches = match_pattern(pattern, msg.text)
     if matches then
-      —print("Msg pattern: ", pattern)
-    if is_plugin_disabled_on_chat(plugin_name, receiver) then
+      print("msg matches: ", pattern)
+
+      if is_plugin_disabled_on_chat(plugin_name, receiver) then
         return nil
       end
-      — Function exists
+      -- Function exists
       if plugin.run then
-        — If plugin is for privileged users only
+        -- If plugin is for privileged users only
         if not warns_user_not_allowed(plugin, msg) then
           local result = plugin.run(msg, matches)
           if result then
@@ -165,28 +168,28 @@ function match_plugin(plugin, plugin_name, msg)
           end
         end
       end
-      — One patterns matches
+      -- One patterns matches
       return
     end
   end
 end
 
-— DEPRECATED, use send_large_msg(destination, text)
+-- DEPRECATED, use send_large_msg(destination, text)
 function _send_msg(destination, text)
   send_large_msg(destination, text)
 end
 
-— Save the content of _config to config.lua
+-- Save the content of _config to config.lua
 function save_config( )
   serialize_to_file(_config, './data/config.lua')
   print ('saved config into ./data/config.lua')
 end
 
-— Returns the config from config.lua file.
-— If file doesn't exist, create it.
+-- Returns the config from config.lua file.
+-- If file doesn't exist, create it.
 function load_config( )
   local f = io.open('./data/config.lua', "r")
-  — If config.lua doesn't exist
+  -- If config.lua doesn't exist
   if not f then
     print ("Created new config file: data/config.lua")
     create_config()
@@ -195,17 +198,14 @@ function load_config( )
   end
   local config = loadfile ("./data/config.lua")()
   for v,user in pairs(config.sudo_users) do
-    print("Sudo: " .. user)
-  end
-  for v,VIP in pairs(config.vip_users) do
-    print("VIP: " .. VIP)
+    print("Sudo user: " .. user)
   end
   return config
 end
 
-— Create a basic config.json file and saves it.
+-- Create a basic config.json file and saves it.
 function create_config( )
-  — A simple config with basic plugins and ourselves as privileged user
+  -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
     "admin",
@@ -229,13 +229,8 @@ function create_config( )
     "powertd",
     "TDplugs"
     },
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {219201071,248974584,173061880,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/td.json'},
-
-    —Start rate:
-    Group_rate = [[]],
-    Supergroup_rate = [[]]
-    —Finish rate.
   }
   serialize_to_file(config, './data/config.lua')
   print('saved config into ./data/config.lua')
@@ -246,21 +241,21 @@ function on_our_id (id)
 end
 
 function on_user_update (user, what)
-  —vardump (user)
+  --vardump (user)
 end
 
 function on_chat_update (chat, what)
-  —vardump (chat)
+  --vardump (chat)
 end
 
 function on_secret_chat_update (schat, what)
-  —vardump (schat)
+  --vardump (schat)
 end
 
 function on_get_difference_end ()
 end
 
-— Enable plugins in config.json
+-- Enable plugins in config.json
 function load_plugins()
   for k, v in pairs(_config.enabled_plugins) do
     print("Loading plugin", v)
@@ -279,7 +274,7 @@ function load_plugins()
   end
 end
 
-— custom add
+-- custom add
 function load_data(filename)
 
   local f = io.open(filename)
@@ -304,21 +299,21 @@ function save_data(filename, data)
 end
 
 
-— Call and postpone execution for cron plugins
+-- Call and postpone execution for cron plugins
 function cron_plugins()
 
   for name, plugin in pairs(plugins) do
-    — Only plugins with cron function
+    -- Only plugins with cron function
     if plugin.cron ~= nil then
       plugin.cron()
     end
   end
 
-  — Called again in 2 mins
+  -- Called again in 2 mins
   postpone (cron_plugins, false, 120)
 end
 
-— Start and load values
+-- Start and load values
 our_id = 0
 now = os.time()
 math.randomseed(now)
