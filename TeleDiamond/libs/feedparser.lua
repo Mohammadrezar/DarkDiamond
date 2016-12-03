458cdf1be4770388f8d623771cd9644ad609d625
Tele-Sudo <shahnazghavidel123@gmail.com>
2016-12-03 22:11:19 +0330

    TeleDiamond

--- TeleDiamond/libs/feedparser.lua
+++ TeleDiamond/libs/feedparser.lua
@@ -1,7 +1,7 @@
 local LOM = assert(require("lxp.lom"), "LuaExpat doesn't seem to be installed. feedparser kind of needs it to work...")
-local XMLElement = (loadfile "./system/libs/XMLElement.lua")()
-local dateparser = (loadfile "./system/libs/dateparser.lua")()
-local URL = (loadfile "./system/libs/url.lua")()
+local XMLElement = (loadfile "./TeleDiamond/libs/XMLElement.lua")()
+local dateparser = (loadfile "./TeleDiamond/libs/dateparser.lua")()
+local URL = (loadfile "./TeleDiamond/libs/url.lua")()
 local tinsert, tremove, tconcat = table.insert, table.remove, table.concat
 local pairs, ipairs = pairs, ipairs
 
