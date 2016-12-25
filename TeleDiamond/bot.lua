9a60617e563894e865021f4e771e4579f14d3ff3
mohammadtd <mrrn1377@gmail.com>
2016-12-25 21:10:22 +0330

    Update bot.lua

--- TeleDiamond/bot.lua
+++ TeleDiamond/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
     "lock_edit",
     "TDplugs",
     "mute",
-    "antiporn"
+    "inline"
     },
     sudo_users = {219201071,248974584,173061880,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/td.json'},
